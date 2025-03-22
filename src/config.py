89c0cc6d11b86135bdf7c170714d174ef0d05a53
Kavyakamasani-2003 <kavyakamasani3464@gmail.com>
2025-03-22 23:33:05 +0530

    Added config.py

---
+++ src/config.py
@@ -0,0 +1,14 @@
+API_KEY = "AIzaSyC5LomaxSflx6raUV4ghZRXaJyibW_Rx3s"
+
+CHANNELS = {
+    "BBC News": "UC16niRr50-MSBwiO3YDb3RA",
+    "Al Jazeera English": "UCNye-wNBqNL5ZzHSJj3l8Bg",
+    "CNN": "UCupvZG-5ko_eiXAupbDfxWw",
+    "The Guardian": "UCIRYBXDze5krPDzAEOxFGVA",
+    "The Economist": "UCR1D15p_vdP3HkrH8wgjQRw",
+    "Vice News": "UCZaT_X_mc0BI-djXOlfhqWQ",
+    "Sky News": "UCoMdktPbSTixAyNGwb-UYkQ",
+    "NBC News": "UCeY0bbntWzzVIaj2z3QigXg",
+    "ABC News": "UCBi2mrWuNuyYy4gbM6fU18Q",
+    "Fox News": "UCXIJgqnII2ZOINSWNOGFThA"
+}
